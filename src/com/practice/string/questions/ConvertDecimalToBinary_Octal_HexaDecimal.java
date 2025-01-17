0cd48380389672c3d19c8a8f8c2dba4b421f4981
Chougule <sourabhchougule264@gmail.com>
2025-01-17 10:48:50 +0530

    Convert Decimal to Binary ,Octal and HexaDecimal code

--- src/com/practice/string/questions/ConvertDecimalToBinary_Octal_HexaDecimal.java
+++ src/com/practice/string/questions/ConvertDecimalToBinary_Octal_HexaDecimal.java
@@ -20,41 +20,41 @@ public class ConvertDecimalToBinary_Octal_HexaDecimal {
 	private static void convertDecimalToHexaDecimal(int inputNum) {
 		// TODO Auto-generated method stub
 		String hexaDecNum = "";
-		char hexaDecimals[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-		while(inputNum > 0) {
+		char hexaDecimals[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+		while (inputNum > 0) {
 			int digit = inputNum % 16;
 			hexaDecNum = hexaDecimals[digit] + hexaDecNum;
 			inputNum = inputNum / 16;
 		}
-		
-		System.out.println("The HexaDecimal is :" +hexaDecNum);
+
+		System.out.println("The HexaDecimal is :" + hexaDecNum);
 	}
 
 	private static void convertDecimalToOctal(int inputNum) {
 		// TODO Auto-generated method stub
-		String binaryNum = "";
-		
-		while(inputNum > 0) {
+		String octalNum = "";
+
+		while (inputNum > 0) {
 			int digit = inputNum % 8;
-			binaryNum = digit + binaryNum;
+			octalNum = digit + octalNum;
 			inputNum = inputNum / 8;
 		}
-		
-		System.out.println("The Binary is :" +binaryNum);
+
+		System.out.println("The Binary is :" + octalNum);
 	}
 
 	private static void convertDecimalToBinary(int inputNum) {
 		// TODO Auto-generated method stub
 		// To convert the decimal to binary, take each remainder of number by dividing 2
-		String octalNum = "";
+		String binaryNum = "";
 		while (inputNum > 0) {
 			int digit = inputNum % 2;
-			octalNum = digit + octalNum;
+			binaryNum = digit + binaryNum;
 			inputNum = inputNum / 2;
 
 		}
-		
-		System.out.println("The Octal is :" +octalNum);
+
+		System.out.println("The Octal is :" + binaryNum);
 	}
 
 }
